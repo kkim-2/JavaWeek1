721e697e7bc0cedd37e846bb11fc5875675cf7d1
kkim-2 <kimkailyn247@gmail.com>
2025-02-11 10:10:18 -0500

    Create comments.java

---
+++ comments.java
@@ -0,0 +1,17 @@
+/******************************
+ * project: Demo for Comments
+ * Programmer: Kailyn
+ * Date: Febuary 11
+ * Program Name: Comments.java
+ *******************************/
+import java.io.*;
+public class Comments
+{
+    public static void main(String[]args)
+    {
+        System.out.println("Hi") //message to user
+        System.out.println("LabOneA"); //assignment
+    }
+}
+/* to write detailed comment wraps around screen, use this block style 
+of coding */
