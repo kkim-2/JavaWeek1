30e45064e7ec6f3128e57a91caaf3b9f1be365f9
kkim-2 <kimkailyn247@gmail.com>
2025-02-11 10:16:52 -0500

    Update comments.java

--- comments.java
+++ comments.java
@@ -5,11 +5,11 @@
  * Program Name: Comments.java
  *******************************/
 import java.io.*;
-public class Comments
+public class comments
 {
     public static void main(String[]args)
     {
-        System.out.println("Hi") //message to user
+        System.out.println("Hi"); //message to user
         System.out.println("LabOneA"); //assignment
     }
 }
